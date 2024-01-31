94a759858058e1f1939bfb2e26fb6b60238a28fd
Bader Sanad Alanazi <alanazibs@rcjy.gov.sa>
2024-01-31 09:30:51 +0300

    Add RoleControllerV3

---
+++ src/main/java/com/bader88/springsecurity/controller/RoleControllerV4.java
@@ -0,0 +1,47 @@
+package com.bader88.springsecurity.controller;
+
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/role/v3") // Base mapping for all endpoints in this controller
+public class RoleControllerV3 {
+
+    @GetMapping("/guest")
+    public String AnyOneCanCallMe() {
+        return "welcome guest"; // Returns a welcome message for any one
+    }
+
+    @GetMapping("/admin/{username}")
+    @RolesAllowed({"ADMIN"})
+    public String JustAdminWithRealUsernameCanCallMe(@PathVariable String username) {
+        return "welcome : " + username; // Returns a welcome message for admin users
+    }
+
+    @GetMapping("/Specific-admin/{username}")
+    @RolesAllowed({"ADMIN"})
+    @PostAuthorize("returnObject == 'root'") // Specifies post-authorization rules, ensuring that the returned value equals 'root'
+    public String JustSpecificAdminCanCallMe(@PathVariable String username) {
+        return username;
+    }
+
+
+    @GetMapping("/user/{username}")
+    @RolesAllowed({"USER"})
+    public String JustUserWithRealUsernameCanCallMe(@PathVariable String username) {
+        return "welcome : " + username; // Returns a welcome message for regular users
+    }
+
+    @GetMapping("/registered/{username}")
+    @RolesAllowed({"USER", "ADMIN"})
+    public String AllUsersCanCallMe(@PathVariable String username, Authentication authentication) {
+        return "You are registered as     : " + authentication.getName() // Returns the currently authenticated user's name
+                + "\nand your path variable is : " + username; // Returns the path variable 'username'
+    }
+}
