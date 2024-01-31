1ccf08d2a2a843f55b9b46c960a9060bd0e70143
Bader Sanad Alanazi <alanazibs@rcjy.gov.sa>
2024-01-31 11:22:25 +0300

    add @EnableMethodSecurity(jsr250Enabled = true,securedEnabled = true) to enable JSR-250 annotation and Secured annotation

--- src/main/java/com/bader88/springsecurity/configuration/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/bader88/springsecurity/configuration/basic/BasicAuthSecurityConfiguration.java
@@ -27,7 +27,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
  */
 
 @Configuration
-@EnableMethodSecurity
+@EnableMethodSecurity(jsr250Enabled = true,securedEnabled = true)
 public class BasicAuthSecurityConfiguration {
 
     /**
